0f6b8675765507347fb093198d31953875467320
Sandali <sanwije2@gmail.com>
2021-04-23 01:59:02 +0530

    DB file edited.

--- UserService/gadgetbadget.sql
+++ UserService/gadgetbadget.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Apr 21, 2021 at 09:30 PM
+-- Generation Time: Apr 22, 2021 at 10:24 PM
 -- Server version: 10.4.14-MariaDB
 -- PHP Version: 7.4.9
 
@@ -24,23 +24,6 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
--- Table structure for table `users`
---
-
-CREATE TABLE `users` (
-  `U_id` int(30) NOT NULL,
-  `fname` varchar(250) NOT NULL,
-  `lname` varchar(250) NOT NULL,
-  `nic` varchar(250) NOT NULL,
-  `address` varchar(250) NOT NULL,
-  `phone` varchar(250) NOT NULL,
-  `email` varchar(250) NOT NULL,
-  `username` varchar(250) NOT NULL,
-  `password` varchar(250) NOT NULL,
-  `type` varchar(40) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-
---
 -- Dumping data for table `users`
 --
 
@@ -48,9 +31,8 @@ INSERT INTO `users` (`U_id`, `fname`, `lname`, `nic`, `address`, `phone`, `email
 (11, 'Sandali', 'Wijekoon', '990234780V', 'No 26,Aluvihare rd,Matale', '0778996678', 'san1999@gmail.com', 'sandali', 'c2FuZGFsaTk5', 'Seller'),
 (15, 'Sam', 'Perera', '900233790V', 'No 23,Peradeniya rd,Kandy', '0716789118', 'samp@gmail.com', 'admin', 'YWRtaW4xMjM=', 'Admin'),
 (16, 'Yohani', 'Silva', '932343790V', 'No 43,Katugastota rd,Kurunegala', '0756345608', 'yohasil@gmail.com', 'yohani', 'eW9oYW5pMTY=', 'Researcher'),
-(19, 'Anne', 'Peries', '962135460V', 'No 51,Kandy rd,Colombo', '0711196608', 'anneper96@gmail.com', 'anne', 'YW5uZXA5Ng==', 'Buyer'),
-(30, 'Pasan', 'Dissanayaka', '945456780V', 'No 02,Pilimathalawa rd,Kandy', '0778908678', 'pasdiss@gmail.com', 'pasan', 'cGFzYW4zMw==', 'Seller'),
-(32, 'Rashini', 'Weerakoon', '984825467V', 'No 21,Kandy rd,Kegalle', '0714534528', 'rashweer@gmail.com', 'rashini', 'cmFzaGk5OA==', 'Buyer');
+(19, 'Anne', 'Peries', '962135460V', 'No 51,Kandy rd,Colombo', '0711196608', 'aneper96@gmail.com', 'anne', 'YW5uZXA5Ng==', 'Buyer'),
+(32, 'Rashini', 'Weerakoon', '984825467V', 'No 21,Kandy rd,Kegalle', '0714534528', 'rashweer@gmail.com', 'rashini', 'cmFzaGk5OA==', 'Seller');
 
 --
 -- Indexes for dumped tables
@@ -70,7 +52,7 @@ ALTER TABLE `users`
 -- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
-  MODIFY `U_id` int(30) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=33;
+  MODIFY `U_id` int(30) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=46;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
